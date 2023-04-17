6243c928b8d2077e71ea95557c963780cbefe13c
saigetset <krishmaddali77@gmail.com>
2023-04-17 10:21:57 -0500

    first commit

---
+++ Arrays/src/com/arrays/sai/LaunchArrays.java
@@ -0,0 +1,36 @@
+package com.arrays.sai;
+class A{
+	private int a;
+	private int b;
+	
+	public int getA() {
+		return a;
+	}
+	public void setA(int a) {
+		this.a = a;
+	}
+	public int getB() {
+		return b;
+	}
+	public void setB(int b) {
+		this.b = b;
+	}
+}
+
+public class LaunchArrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr= {1,2,3,4,5};
+		for(int i:arr) {
+			System.out.print(i+" ");
+		}
+		System.out.println("Hello");
+		A a=new A();
+		a.setA(10);
+		System.out.println(a.getA());
+		a.setB(20);
+		System.out.println(a.getB());
+	}
+
+}
